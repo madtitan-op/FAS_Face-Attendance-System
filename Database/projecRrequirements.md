48be650bfcd54538ab8bfab4fb7b78a5e5ffeab4
Animesh Mahata <imanimesh1210@gmail.com>
2024-09-13 13:03:27 +0530

    added functions to insert, update, fetch and delete data to and from student and faculty db

--- Database/projecRrequirements.md
+++ Database/projecRrequirements.md
@@ -7,13 +7,25 @@
     - `Student Last Name`
 - Name the table as `{dept_name}_p{passout_year}_students` (eg: `cse_p2020_students`)
 
+| s_id | roll   | firstname | lastname |
+|:----:|:------:|:---------:|:--------:|
+| 1    | 121001 | _John_    | _Doe_    |
+| 2    | 121002 | _Jane_    | _Doe_    |
+
 ### For Faculties
 - Create a new table for faculty details
     - `Faculty ID (auto-generated primary key)`
+    - `Faculty Serial Number`
     - `Faculty First Name`
     - `Faculty Last Name`
     - `Faculty Department`
 - Name the table as `faculties`
+
+| f_id | f_serial | firstname | lastname | department |
+|:----:|:--------:|:---------:|:--------:|:----------:|
+|   1  |   90101  |   _John_  |  _Smith_ |     CSE    |
+|   2  |   90102  |  _Olivia_ |  _Smith_ |     ECE    |
+|      |          |           |          |            |
 
 <br><br>
 
@@ -21,8 +33,10 @@
 ### For Students
 - `Insert new students into the database - for new admissions`
 - `Delete students from the database - for students who have left`
+- `Update student details - for students who have changed their details`
 
 ### For Faculties
 - `Insert new faculties into the database - for new faculties`
 - `Delete faculties from the database - for faculties who have left`
+- `Update faculty details - for faculties who have changed their details`
 
--- Database/tables.sql
+++ Database/tables.sql
@@ -7,6 +7,7 @@ CREATE TABLE csep2025(
    passyear NUMERIC(4,0) NOT NULL
 );
 
+-- Create a student table wrt dept and passout year
 CREATE TABLE {dept_name}_p{passout_year}_students (
    s_id SERIAL PRIMARY KEY,
    s_roll INT UNIQUE,
@@ -14,8 +15,10 @@ CREATE TABLE {dept_name}_p{passout_year}_students (
    lastname VARCHAR(50)
 );
 
+-- Delete a student table wrt dept and passout year
 DROP TABLE {dept_name}_p{passout_year}_students;
 
+-- Create faculty table
 CREATE TABLE faculties (
    f_id SERIAL PRIMARY KEY,
    f_serial INT UNIQUE,
@@ -24,4 +27,42 @@ CREATE TABLE faculties (
    department VARCHAR(50)
 );
 
+-- Delete faculty table
 DROP TABLE faculties;
+
+-- Insert a student
+INSERT INTO {dept_name}_p{passout_year}_students (s_roll, firstname, lastname)
+VALUES ({roll}, '{firstname}', '{lastname}');
+
+-- Delete a student
+DELETE FROM {dept_name}_p{passout_year}_students WHERE s_roll = {roll};
+
+-- Update a student
+UPDATE {dept_name}_p{passout_year}_students
+SET firstname = {firstname}, lastname = {lastname}
+WHERE s_roll = {roll};
+
+-- Fetch a student
+SELECT * FROM {dept_name}_p{passout_year}_students WHERE s_roll = {roll};
+
+-- Fetch all students
+SELECT * FROM {dept_name}_p{passout_year}_students;
+
+
+-- Insert a faculty
+INSERT INTO faculties (f_serial, firstname, lastname, department)
+VALUES ({serial}, {firstname}, {lastname}, {dept});
+
+-- Delete a faculty
+DELETE FROM faculties WHERE f_serial = {serial};
+
+-- Update a faculty
+UPDATE faculties
+SET firstname = {firstname}, lastname = {lastname}, department = {dept}
+WHERE f_serial = {serial};
+
+-- Fetch a faculty
+SELECT * FROM faculties WHERE f_serial = {serial};
+
+-- Fetch all faculties
+SELECT * FROM faculties;
